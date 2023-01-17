2c2fcfbd19d02491d5ca0337a6457a3cd642909a
higuma-san <mi-obara@dgic.co.jp>
2023-01-17 16:14:19 +0900

    プロジェクト ファイルを追加します。

---
+++ etc/Etc.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace etc
+{
+    internal class Etc
+    {
+        public int a;
+        public int b;
+
+        public void aa()
+        {
+            a++;
+        }
+
+        public void bb()
+        {
+            b++;
+        }
+
+    }
+}
---
+++ etc/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace etc
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Etc cc = new Etc();
+
+            cc.a = 2;
+            cc.aa();
+
+            Etc dd = new Etc();
+
+            dd.b= 3;
+            dd.bb();
+        }
+    }
+}
